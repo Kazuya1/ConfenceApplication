0fb146fd01dd4d8c51b086a765c908b3c505626d
Xinghao Cheng <chengxinghao93@gmail.com>
2017-01-27 01:03:33 -0500

    almost done

--- confclient.c
+++ confclient.c
@@ -4,14 +4,14 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <string.h>
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <netinet/in.h> 
-#include <arpa/inet.h> 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <netdb.h>
-#include <time.h> 
+#include <time.h>
 #include <errno.h>
-
+#include <unistd.h>
 #include <stdlib.h>
 
 #define MAXMSGLEN  1024
@@ -24,65 +24,68 @@ extern int hooktoserver(char *servhost, ushort servport);
 
 /*--------------------------------------------------------------------*/
 int main(int argc, char *argv[]) {
-	int sock;
-
-	fd_set rfds;
-	int retval;
-
-	/* check usage */
-	if (argc != 3) {
-		fprintf(stderr, "usage : %s <servhost> <servport>\n", argv[0]);
-		exit(1);
-	}
-
-	/* get hooked on to the server */
-	sock = hooktoserver(argv[1], atoi(argv[2]));
-	if (sock == -1) {
-		perror("Error: ");
-		exit(1);
-	}
-
-	FD_ZERO(&rfds);
-
-	FD_SET(0, &rfds);
-	FD_SET(sock, &rfds);
-
-	/* keep talking */
-	while (1) {
-
-		/*
-		 FILL HERE
-		 use select() to watch simultaneously for
-		 inputs from user and messages from server
-		 */
-
-
-		if (/* FILL HERE: message from server? */) {
-			char *msg;
-			msg = recvtext(sock);
-			if (!msg) {
-				/* server killed, exit */
-				fprintf(stderr, "error: server died\n");
-				exit(1);
-			}
-
-			/* display the message */
-			printf(">>> %s", msg);
-			/* free the message */
-			free(msg);
-		}
-
-		if (/* FILL HERE: input from keyboard? */) {
-			char msg[MAXMSGLEN];
-			if (!fgets(msg, MAXMSGLEN, stdin))
-				exit(0);
-			sendtext(sock, msg);
-		}
-
-		printf(">");
-		fflush(stdout);
-	}
-	return 0;
+    int sock;
+    fd_set tmp;
+    fd_set rfds;
+    int retval;
+    
+    /* check usage */
+    if (argc != 3) {
+        fprintf(stderr, "usage : %s <servhost> <servport>\n", argv[0]);
+        exit(1);
+    }
+    
+    /* get hooked on to the server */
+    sock = hooktoserver(argv[1], atoi(argv[2]));
+    if (sock == -1) {
+        perror("Error: ");
+        exit(1);
+    }
+    
+    FD_ZERO(&rfds);
+    FD_SET(0, &rfds);
+    FD_SET(sock, &rfds);
+    
+    /* keep talking */
+    while (1) {
+    FD_ZERO(&rfds);
+    FD_SET(0, &rfds);
+    FD_SET(sock, &rfds);
+        /*
+         FILL HERE
+         use select() to watch simultaneously for
+         inputs from user and messages from server
+         */
+        select(sock+1, &rfds, NULL, NULL, NULL);
+//        printf("%d\n",retval);
+        
+//       printf("here\n");
+        if (FD_ISSET(sock, &rfds)){
+            char *msg;
+            msg = recvtext(sock);
+            if (!msg) {
+                /* server killed, exit */
+                fprintf(stderr, "error: server died\n");
+                exit(1);
+            }
+            
+            /* display the message */
+            printf(">>> %s", msg);
+            /* free the message */
+            free(msg);
+        }
+        
+        if (FD_ISSET(0, &rfds)) {
+            char msg[MAXMSGLEN];
+            if (!fgets(msg, MAXMSGLEN, stdin))
+                exit(0);
+            sendtext(sock, msg);
+        }
+        
+        printf(">");
+        fflush(stdout);
+    }
+    return 0;
 }
 /*--------------------------------------------------------------------*/
 
--- confserver.c
+++ confserver.c
@@ -4,14 +4,14 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <string.h>
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <netinet/in.h> 
-#include <arpa/inet.h> 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <netdb.h>
-#include <time.h> 
+#include <time.h>
 #include <errno.h>
-
+#include <unistd.h>
 #include <stdlib.h>
 
 extern char * recvtext(int sd);
@@ -22,133 +22,192 @@ extern int startserver();
 
 /*--------------------------------------------------------------------*/
 int fd_isset(int fd, fd_set *fsp) {
-	return FD_ISSET(fd, fsp);
+    return FD_ISSET(fd, fsp);
 }
 /* main routine */
 int main(int argc, char *argv[]) {
-	int servsock; /* server socket descriptor */
-
-	fd_set livesdset; /* set of live client sockets */
-	int livesdmax; /* largest live client socket descriptor */
-
-	/* check usage */
-	if (argc != 1) {
-		fprintf(stderr, "usage : %s\n", argv[0]);
-		exit(1);
-	}
-
-	/* get ready to receive requests */
-	servsock = startserver();
-	if (servsock == -1) {
-		perror("Error on starting server: ");
-		exit(1);
-	}
-
-	/*
-	 FILL HERE:
-	 init the set of live clients
-	 */
-
-
-	/* receive requests and process them */
-	while (1) {
-		int frsock; /* loop variable */
-
-		/*
-		 FILL HERE
-		 wait using select() for
-		 messages from existing clients and
-		 connect requests from new clients
-		 */
-
-
-		/* look for messages from live clients */
-		for (frsock = 3; frsock <= livesdmax; frsock++) {
-			/* skip the listen socket */
-			/* this case is covered separately */
-			if (frsock == servsock)
-				continue;
-
-			if ( /* FILL HERE: message from client 'frsock'? */ ) {
-				char * clienthost; /* host name of the client */
-				ushort clientport; /* port number of the client */
-
-
-				/*
-				 FILL HERE:
-				 figure out client's host name and port
-				 using getpeername() and gethostbyaddr()
-				 */
-
-
-				/* read the message */
-				msg = recvtext(frsock);
-				if (!msg) {
-					/* disconnect from client */
-					printf("admin: disconnect from '%s(%hu)'\n", clienthost,
-							clientport);
-
-					/*
-					 FILL HERE:
-					 remove this guy from the set of live clients
-					 */
-
-
-					/* close the socket */
-					close(frsock);
-				} else {
-					/*
-					 FILL HERE
-					 send the message to all live clients
-					 except the one that sent the message
-					 */
-
-
-					/* display the message */
-					printf("%s(%hu): %s", clienthost, clientport, msg);
-
-					/* free the message */
-					free(msg);
-				}
-			}
-		}
-
-		/* look for connect requests */
-		if ( /* FILL HERE: connect request from a new client? */ ) {
-
-			/*
-			 FILL HERE:
-			 accept a new connection request
-			 */
-
-
-			/* if accept is fine? */
-			if (csd != -1) {
-				char * clienthost; /* host name of the client */
-				ushort clientport; /* port number of the client */
-
-				/*
-				 FILL HERE:
-				 figure out client's host name and port
-				 using gethostbyaddr() and without using getpeername().
-				 */
-
-				printf("admin: connect from '%s' at '%hu'\n", clienthost,
-						clientport);
-
-				/*
-				 FILL HERE:
-				 add this guy to set of live clients
-				 */
-
-
-			} else {
-				perror("accept");
-				exit(0);
-			}
-		}
-	}
-	return 0;
+    int servsock; /* server socket descriptor */
+    fd_set tmp;
+    fd_set livesdset; /* set of live client sockets */
+    int livesdmax; /* largest live client socket descriptor */
+    
+    /* check usage */
+    if (argc != 1) {
+        fprintf(stderr, "usage : %s\n", argv[0]);
+        exit(1);
+    }
+    
+    /* get ready to receive requests */
+    servsock = startserver();
+    if (servsock == -1) {
+        perror("Error on starting server: ");
+        exit(1);
+    }
+    
+    /*
+     FILL HERE:
+     init the set of live clients
+     */
+    
+    int client[FD_SETSIZE];
+    int maxindex = -1;
+    int i;
+    for (i = 0; i < FD_SETSIZE; i++)
+        client[i] = -1;
+    fd_set rset;
+    livesdmax = servsock;
+    FD_ZERO(&livesdset);
+    FD_SET(servsock, &livesdset);
+        
+    /* receive requests and process them */
+    while (1) {
+        int frsock; /* loop variable */
+        tmp = livesdset;
+        /*
+         FILL HERE
+         wait using select() for
+         messages from existing clients and
+         connect requests from new clients
+         */
+        int sv = select(livesdmax+1, &tmp, NULL, NULL, NULL);
+       // printf("%d\n",sv);
+        if(sv==-1){
+            perror("select");
+            exit(1);
+        }
+        /* look for messages from live clients */
+        for (frsock = 3; frsock <= livesdmax; frsock++) {
+            /* skip the listen socket */
+            /* this case is covered separately */
+            if (frsock == servsock){
+ //               printf("here\n");
+                continue;
+            }
+            
+            if (fd_isset(frsock, &tmp)) {
+                char * clienthost; /* host name of the client */
+                ushort clientport; /* port number of the client */
+                
+                
+                /*
+                 FILL HERE:
+                 figure out client's host name and port
+                 using getpeername() and gethostbyaddr()
+                 */
+                struct sockaddr_in sa;
+                unsigned int salen = sizeof(struct sockaddr_in);
+                if(getpeername(frsock, (struct sockaddr*)&sa, &salen)==-1){
+                    perror("getpeername");
+                    exit(1);
+                }
+                struct hostent* ret = gethostbyaddr((void*)&sa.sin_addr.s_addr, 4, AF_INET);
+                clienthost = ret->h_name;
+                clientport = ntohs(sa.sin_port);
+                char* msg = NULL;
+                /* read the message */
+                msg = recvtext(frsock);
+                if (!msg) {
+                    /* disconnect from client */
+                    printf("admin: disconnect from '%s(%hu)'\n", clienthost,
+                           clientport);
+                    
+                    /*
+                     FILL HERE:
+                     remove this guy from the set of live clients
+                     */
+                    FD_CLR(frsock, &livesdset);
+                    for(i=0;i<FD_SETSIZE;i++){
+                        if(frsock==client[i]){
+                            client[i] = -1;
+                        }
+                    }
+                    if(frsock==livesdmax){
+                        livesdmax = servsock;
+                        for(i=0;i<FD_SETSIZE;i++){
+                            if(client[i]>livesdmax){
+                                livesdmax = client[i];
+                            }
+                        }
+                    }
+                    
+                    /* close the socket */
+                    close(frsock);
+                } else {
+                    /*
+                     FILL HERE
+                     send the message to all live clients
+                     except the one that sent the message
+                     */
+                    for(i=0;i<FD_SETSIZE;i++){
+                        if(client[i]>0&&client[i]!=frsock){
+                            sendtext(client[i], msg);
+                        }
+                    }
+                    
+                    /* display the message */
+                    printf("%s(%hu): %s", clienthost, clientport, msg);
+                    
+                    /* free the message */
+                    free(msg);
+                }
+            }
+        }
+        
+        /* look for connect requests */
+        if (fd_isset(servsock, &tmp)) {
+            
+            /*
+             FILL HERE:
+             accept a new connection request
+             */
+            struct sockaddr_in sa;
+            unsigned int salen = sizeof(struct sockaddr);
+            int csd = accept(servsock,(struct sockaddr*)&sa,&salen);
+            
+            /* if accept is fine? */
+            if (csd != -1) {
+                char * clienthost; /* host name of the client */
+                ushort clientport; /* port number of the client */
+                /*
+                 FILL HERE:
+                 figure out client's host name and port
+                 using gethostbyaddr() and without using getpeername().
+                 */
+                
+                struct hostent* ret = gethostbyaddr((const void*)&sa.sin_addr, sizeof(struct in_addr), AF_INET);
+                if(ret==NULL){
+                    perror("gethostbyaddr");
+                    exit(1);
+                }
+                clienthost = ret->h_name;
+                clientport = ntohs(sa.sin_port);
+                printf("admin: connect from '%s' at '%hu'\n", clienthost,
+                       clientport);
+                
+                /*
+                 FILL HERE:
+                 add this guy to set of live clients
+                 */
+                for(i=0;i<FD_SETSIZE;i++){
+                    if(client[i]<0){
+                        client[i] = csd;
+                        break;
+                    }
+                }
+                if(i==FD_SETSIZE){
+                    printf("Too many clients\n");
+                    exit(1);
+                }
+                FD_SET(csd,&livesdset);
+                if(i>maxindex) maxindex = i;
+                if(csd>livesdmax) livesdmax = csd;
+            } else {
+                perror("accept");
+                exit(0);
+            }
+        }
+    }
+    return 0;
 }
 /*--------------------------------------------------------------------*/
 
